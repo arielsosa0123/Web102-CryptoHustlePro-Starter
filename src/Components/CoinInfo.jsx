057cdccb312854afea7449f00c589ce0bdd1afb8
Andrew Burke <andrew@codepath.org>
2026-05-14 09:12:00 -0700

    Update CoinInfo.jsx

--- src/Components/CoinInfo.jsx
+++ src/Components/CoinInfo.jsx
@@ -1,32 +1,15 @@
-import { useEffect, useState } from "react"
-const API_KEY = import.meta.env.VITE_APP_API_KEY
-
-const CoinInfo = ({ image, name, symbol }) => {
-  const [price, setPrice] = useState(null)
-
-  useEffect(() => {
-    const getCoinPrice = async () => {
-      const response = await fetch(
-        `https://min-api.cryptocompare.com/data/price?fsym=${symbol}&tsyms=USD&api_key=` +
-          API_KEY
-      )
-      const json = await response.json()
-      setPrice(json)
-    }
-    getCoinPrice().catch(console.error)
-  }, [symbol])
-
+const CoinInfo = ({ id, image, name, symbol, price }) => {
   return (
-    <li className="main-list" key={symbol}>
+    <li className="main-list" key={id}>
       <img
         className="icons"
-        src={`https://www.cryptocompare.com${image}`}
+        src={image}
         alt={`Small icon for ${name} crypto coin`}
       />
       {name} <span className="tab"></span>
-      {price && price.USD ? ` $${price.USD} USD` : null}
+      {price != null ? ` $${price} USD` : null}
     </li>
   )
 }
-
+ 
 export default CoinInfo
